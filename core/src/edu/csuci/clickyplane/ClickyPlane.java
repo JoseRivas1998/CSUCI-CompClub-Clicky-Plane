b5cf8e7eccb7fd62c8914b5ab549c118b995f04b
Jose Rodriguez Rivas <JoseRivas823@gmail.com>
2019-10-18 17:04:30 -0700

    Day 1 - GameState and Abstract Entity

--- core/src/edu/csuci/clickyplane/ClickyPlane.java
+++ core/src/edu/csuci/clickyplane/ClickyPlane.java
@@ -5,29 +5,33 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import edu.csuci.clickyplane.gamestates.GameStateType;
+import edu.csuci.clickyplane.managers.GameStateManager;
 
 public class ClickyPlane extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
+
+	private GameStateManager gsm;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		this.gsm = new GameStateManager(GameStateType.PLAY);
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		float dt = Gdx.graphics.getDeltaTime();
+		this.gsm.step(dt);
 	}
-	
+
+	@Override
+	public void resize(int width, int height) {
+		this.gsm.resize(width, height);
+	}
+
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		this.gsm.dispose();
 	}
 }
---
+++ core/src/edu/csuci/clickyplane/gamestates/PlayState.java
@@ -0,0 +1,42 @@
+package edu.csuci.clickyplane.gamestates;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import edu.csuci.clickyplane.managers.GameStateManager;
+
+public class PlayState extends AbstractGameState {
+
+    public PlayState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    public void handleInput(float dt) {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void draw(float dt, SpriteBatch sb, ShapeRenderer sr) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
